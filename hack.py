b7a8941427dce347d2ab7ce86387ab55b203fb59
gilibertojairo-lgtm <gilibertojairo@gmail.com>
2026-04-05 18:51:35 +0000

    holaa

--- hack.py
+++ hack.py
@@ -1,5 +1,8 @@
+import time
 numero = int(input("elige una (rapido): "))
 if numero % 2 == 0:
     print("el", numero, "es par")
 else:
     print("el", numero, "es impar")
+time.sleep(5)
+print("gracias por tu atencio negro conchetumare")
